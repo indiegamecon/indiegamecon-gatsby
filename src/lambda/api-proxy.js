c65034b23acacce0f411512ac33e3d146b0848e3
Greg McKelvey <mckelveygreg@gmail.com>
2019-01-09 14:14:27 -0800

    airtable api setup

---
+++ src/lambda/api-proxy.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+require("dotenv").config();
+
+export function handler(event, context, callback) {
+  console.log(event);
+  if (event.httpMethod === "PATCH") {
+    const body = JSON.parse(event.body);
+    const { params, data } = body;
+    axios({
+      method: "patch",
+      url: params.url,
+      headers: {
+        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+        "Content-type": "application/json"
+      },
+      data: {
+        fields: {
+          "On Display?": data.fields["On Display?"]
+        }
+      }
+    })
+      .then(response => {
+        console.log(response);
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({ ...response.data })
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        callback(null, {
+          statusCode: err.response.status,
+          body: JSON.stringify({ ...err.response.data })
+        });
+      });
+  }
+
+  axios
+    .get(event.queryStringParameters.url, {
+      headers: {
+        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
+      }
+    })
+    .then(response => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({ ...response.data })
+      });
+    })
+    .catch(err => {
+      callback(null, {
+        statusCode: err.response.status,
+        body: JSON.stringify({ ...err.response.data })
+      });
+    });
+}
