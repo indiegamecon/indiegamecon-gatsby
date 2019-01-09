ce33dec347c7b7432a8aa47d2d61eba3a9316372
Greg McKelvey <mckelveygreg@gmail.com>
2019-01-09 13:49:41 -0800

    netlify setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+var proxy = require("http-proxy-middleware")
+
 module.exports = {
   siteMetadata: {
     title: `Indie Game Con 2019`,
@@ -41,4 +43,17 @@ module.exports = {
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
+  // for avoiding CORS while developing Netlify Functions locally
+  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
+  developMiddleware: app => {
+    app.use(
+      "/.netlify/functions/",
+      proxy({
+        target: "http://localhost:9000",
+        pathRewrite: {
+          "/.netlify/functions/": "",
+        },
+      })
+    )
+  },
 }
