402b43c9f000dadad6bb8a838ad0898a3c30f0ee
Greg McKelvey <mckelveygreg@gmail.com>
2019-09-30 18:45:39 -0700

    fix terms button size

--- src/components/gameForm.jsx
+++ src/components/gameForm.jsx
@@ -1,97 +1,9 @@
 import React, { useState } from 'react'
-import { Formik, Form, Field } from 'formik'
+import { Formik, Field } from 'formik'
 import * as yup from 'yup'
-import styled from 'styled-components'
 import { Transition, animated } from 'react-spring/renderprops'
 import { TermsConditions } from './TermsConditions'
-
-const StyledForm = styled(Form)`
-  display: grid;
-  grid-template-columns: 1fr 2fr;
-  grid-gap: 10px;
-  padding: 2rem;
-
-  h3 {
-    text-align: right;
-    line-height: 2rem;
-  }
-  p {
-    background: #2b2b2b55;
-    align-self: center;
-    padding: 10px;
-  }
-  textarea {
-    height: 150px;
-    border-radius: 5px;
-    font-family: inherit;
-    font-weight: bold;
-  }
-  label,
-  legend {
-    text-align: right;
-    font-size: 1.5rem;
-  }
-  input {
-    width: 300px;
-    border-radius: 5px;
-    font-size: 1.5rem;
-  }
-  fieldset input {
-    width: 40px;
-  }
-  .invalid {
-    background: #ff000077;
-  }
-  .terms {
-    text-align: center;
-  }
-  .termsButton {
-    text-decoration: underline;
-    font-size: inherit;
-    background: none;
-    color: #fff200;
-  }
-  .termsCheckbox {
-    height: 2.5rem;
-    width: 2.5rem;
-  }
-  button {
-    margin: 0 auto;
-    width: 250px;
-    grid-column: 1 / 3;
-    background: #2b2b2b;
-    border: none;
-    height: 3rem;
-    border-radius: 5px;
-    box-shadow: 0px 1px 5px #333;
-    color: white;
-    cursor: pointer;
-
-    :disabled {
-      background-color: #aaa;
-    }
-  }
-
-  @media screen and (max-width: 750px) {
-    padding: 0 0 20px 0;
-    grid-template: auto / 1fr;
-
-    input {
-      width: auto;
-    }
-    h3 {
-      font-size: 1.1rem;
-    }
-    label,
-    textarea,
-    input,
-    button,
-    p,
-    legend {
-      font-size: 1rem;
-    }
-  }
-`
+import { StyledForm } from './styledForm'
 
 const GameForm = () => {
   const [formSubmitted, setFormSubmitted] = useState()
