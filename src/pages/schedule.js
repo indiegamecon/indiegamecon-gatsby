4fd6e9a9bf37acc54c2e8de0b73ce31f00eca703
Greg McKelvey <mckelveygreg@gmail.com>
2019-10-30 21:32:09 -0700

    update schedule

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,9 +1,25 @@
 import { graphql, StaticQuery } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Colors } from "../utilities"
+
+const TableWrapper = styled.div`
+  table {
+    border: 2px solid ${Colors.PRIMARY_LIGHT};
+    border-bottom: none;
+    margin: 1rem auto;
+    font-size: 1.25rem;
+  }
+  td,
+  th {
+    border-bottom: 2px solid ${Colors.PRIMARY_LIGHT};
+    padding: 0.5rem;
+  }
+`
 
 const Schedule = () => (
   <Layout>
@@ -17,9 +33,9 @@ const Schedule = () => (
         }
       `}
       render={data => (
-        <>
+        <TableWrapper>
           <MDXRenderer>{data.mdx.body}</MDXRenderer>
-        </>
+        </TableWrapper>
       )}
     />
   </Layout>
